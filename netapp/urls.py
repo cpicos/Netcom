186c07e3c34b170370be65e3fd33f87f1dd65bd7
cpicos <picos.rodriguez.christian@gmail.com>
2019-03-10 14:13:48 -0700

    Testing 2 for smartgit

--- netapp/urls.py
+++ netapp/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("home/", HomeTemplate.as_view(), name="home"),
+    
 ]

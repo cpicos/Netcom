28ace5a512fe4a0462d33f349a29709b0b5250b7
cpicos <picos.rodriguez.christian@gmail.com>
2019-03-10 13:01:00 -0700

    Netcom First Commit

---
+++ netapp/apiviews.py
@@ -0,0 +1,41 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import Http404
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response, redirect
+
+from rest_framework import generics, status, viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class LoginView(APIView):
+    template_name = "login.html"
+    permission_classes = ()
+
+    def get(self, request, ):
+        if request.user is not None:
+            if request.user.is_active:
+                return HttpResponseRedirect('/home/')
+            else:
+                return render_to_response('login.html')
+        else:
+            return render_to_response('login.html')
+
+    def post(self, request, ):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/home/')
+        else:
+            return render_to_response('login.html', {'error': 'Usuario/Contraseña Incorrectos'})
+
+
+class LogoutView(APIView):
+    permission_classes = ()
+
+    def get(self, request, ):
+        logout(request)
+        return redirect('login')
---
+++ netapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from rest_framework.routers import DefaultRouter
+
+from .views import HomeTemplate
+
+from .apiviews import LoginView, LogoutView
+
+
+urlpatterns = [
+    path("", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("home/", HomeTemplate.as_view(), name="home"),
+]
